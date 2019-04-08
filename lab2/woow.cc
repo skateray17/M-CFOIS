00b523c351aea9de0c6583868638cac5dbee42c3
Alexander Kovalchuk <skateray17@gmail.com>
2019-04-08 17:20:41 +0300

    lab2 final

--- lab2/woow.cc
+++ lab2/woow.cc
@@ -28,14 +28,16 @@ std::vector<uint8_t> encr(const uint8_t *key, uint8_t* IV, std::vector<uint8_t>&
 	aes_key_schedule_128(key, roundkeys);
 	uint64_t n = message.size() / 16;
 	auto it = message.begin();
+	aes_encrypt_128(roundkeys, IV, ciphertext);
+	for (int i = 0; i < 16; i++) IV[i] = ciphertext[i];
 	for (uint64_t counter = 0; counter < n; counter++) {
-		aes_encrypt_128(roundkeys, IV, ciphertext);
-		for (size_t i = 0; i < 16; i++) {
-			encryptedMessage.push_back(*(it++) ^ ciphertext[i]);
-		}
 		for (size_t i = 15; i >= 0; --i) {
 			IV[i]++;
 			if (IV[i]) break;
+		}
+		aes_encrypt_128(roundkeys, IV, ciphertext);
+		for (size_t i = 0; i < 16; i++) {
+			encryptedMessage.push_back(*(it++) ^ ciphertext[i]);
 		}
 	}
 	return encryptedMessage;
